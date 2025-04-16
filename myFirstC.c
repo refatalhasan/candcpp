00b45b620266278d62a9081d79f85c02234e70bb
refatalhasan <iamrefatalhasan@gmail.com>
2025-04-16 20:49:28 +0600

    code update

--- myFirstC.c
+++ myFirstC.c
@@ -1,6 +1,21 @@
+// #include <stdio.h>
+
+// int main() {
+//     printf("Hello, World!");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     int a;
+//     a = 5;
+//     printf("The output of the program is %d", a);
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!");
-    return 0;
+int main(){
+    int
 }
